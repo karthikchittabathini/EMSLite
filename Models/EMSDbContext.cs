9761fd6ebb308fa0b36acbdb9d2cd6eb8ef03bd1
karthikchittabathini <chittabathinikarthik@gmail.com>
2023-05-16 16:13:42 +0530

    EMS V1

---
+++ Models/EMSDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EmployeeManagementSystem.Models
+{
+    public partial class EMSDbContext : DbContext
+    {
+        public EMSDbContext()
+        {
+        }
+
+        public EMSDbContext(DbContextOptions<EMSDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=KARTHI\\SQLEXPRESS;Initial Catalog=EMSDB;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.EmployeeId).ValueGeneratedNever();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementSystem.Models
+{
+    [Table("Employee")]
+    public partial class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [StringLength(20)]
+        [Unicode(false)]
+        public string FirstName { get; set; } = null!;
+        [StringLength(20)]
+        [Unicode(false)]
+        public string LastName { get; set; } = null!;
+        [StringLength(20)]
+        [Unicode(false)]
+        public string Designation { get; set; } = null!;
+        [StringLength(20)]
+        [Unicode(false)]
+        public string OfficeLocation { get; set; } = null!;
+        [StringLength(20)]
+        [Unicode(false)]
+        public string MobileNumber { get; set; } = null!;
+        [Column(TypeName = "date")]
+        public DateTime DateOfBirth { get; set; }
+        [StringLength(10)]
+        [Unicode(false)]
+        public string Gender { get; set; } = null!;
+        [StringLength(30)]
+        [Unicode(false)]
+        public string Manager { get; set; } = null!;
+    }
+}
