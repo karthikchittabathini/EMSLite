3062c2e6f2403c2927896132d62ef58c6c46820c
karthikchittabathini <chittabathinikarthik@gmail.com>
2023-05-26 14:27:05 +0530

    Exception handling

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -30,15 +30,26 @@ namespace EmployeeManagementSystem.Controllers
         [HttpGet("GetAllEmployees")]
         public async Task<IActionResult> Index()
         {
-            
-            
-            // Using the stored procedure 
-            var data = _context.Employees.FromSqlRaw<Employee>("EXEC AllEmployees").ToList();
+            try
+            {
+                // Using the stored procedure 
+                var data = _context.Employees.FromSqlRaw<Employee>("EXEC AllEmployees").ToList();
+                
+                return _context.Employees != null ?
+                              View(data) :
+                              Problem("There are no Employees");
 
-          
-            return _context.Employees != null ?
-                          View(data) :
-                          Problem("There are no Employees");
+            }
+            catch(Exception ex)
+            {
+                var model = new ErrorViewModel
+                {
+                    ErrorMessage = ex.Message
+                };
+                return View("Error", model);
+            }
+            
+            
         }
 
 
@@ -46,20 +57,32 @@ namespace EmployeeManagementSystem.Controllers
         [HttpGet("GetEmployeeById/{id}")]
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null || _context.Employees == null)
+            try
             {
-                return NotFound("Not Found");
+                if (id == null || _context.Employees == null)
+                {
+                    return NotFound("Not Found");
+                }
+
+
+                var result = _context.Employees.FromSqlRaw($"exec FindEmployee @p0", id).ToList();
+                var employee = result.Single();
+                if (employee == null)
+                {
+                    return NotFound("Employee Not Found With the Id provided");
+                }
+
+                return View(employee);
+            }
+            catch (Exception ex)
+            {
+                var model = new ErrorViewModel
+                {
+                    ErrorMessage = ex.Message
+                };
+                return View("Error", model);
             }
 
-                
-            var result = _context.Employees.FromSqlRaw($"exec FindEmployee @p0", id).ToList();
-            var employee = result.Single();
-            if (employee == null)
-            {
-                return NotFound("Employee Not Found With the Id provided");
-            }
-
-            return View(employee);
         }
 
 
@@ -78,13 +101,25 @@ namespace EmployeeManagementSystem.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("EmployeeId,FirstName,LastName,Designation,OfficeLocation,MobileNumber,DateOfBirth,Gender,Manager")] Employee employee)
         {
-            if (ModelState.IsValid)
+            try
             {
-                _context.Add(employee);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                if (ModelState.IsValid)
+                {
+                    _context.Add(employee);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(employee);
             }
-            return View(employee);
+            catch (Exception ex)
+            {
+                var model = new ErrorViewModel
+                {
+                    ErrorMessage = ex.Message
+                };
+                return View("Error", model);
+            }
+            
         }
 
 
@@ -93,17 +128,29 @@ namespace EmployeeManagementSystem.Controllers
         [HttpGet("EditEmployee/{id}")]
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null || _context.Employees == null)
+            try
             {
-                return NotFound("Not Found");
-            }
+                if (id == null || _context.Employees == null)
+                {
+                    return NotFound("Not Found");
+                }
 
-            var employee = await _context.Employees.FindAsync(id);
-            if (employee == null)
-            {
-                return NotFound("No Employee Found");
+                var employee = await _context.Employees.FindAsync(id);
+                if (employee == null)
+                {
+                    return NotFound("No Employee Found");
+                }
+                return View(employee);
             }
-            return View(employee);
+            catch (Exception ex)
+            {
+                var model = new ErrorViewModel
+                {
+                    ErrorMessage = ex.Message
+                };
+                return View("Error", model);
+            }
+            
         }
 
 
@@ -114,6 +161,7 @@ namespace EmployeeManagementSystem.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("EmployeeId,FirstName,LastName,Designation,OfficeLocation,MobileNumber,DateOfBirth,Gender,Manager")] Employee employee)
         {
+           
             if (id != employee.EmployeeId)
             {
                 return NotFound("No Employee Found");
@@ -145,19 +193,32 @@ namespace EmployeeManagementSystem.Controllers
         // GET: Employees/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null || _context.Employees == null)
+            try
             {
-                return NotFound("No Employee Found");
-            }
+                if (id == null || _context.Employees == null)
+                {
+                    return NotFound("No Employee Found");
+                }
 
-            var employee = await _context.Employees
-                .FirstOrDefaultAsync(m => m.EmployeeId == id);
-            if (employee == null)
+                var employee = await _context.Employees
+                    .FirstOrDefaultAsync(m => m.EmployeeId == id);
+                if (employee == null)
+                {
+                    return NotFound("No Employee Found");
+                }
+
+                return View(employee);
+
+            }
+            catch (Exception ex)
             {
-                return NotFound("No Employee Found");
+                var model = new ErrorViewModel
+                {
+                    ErrorMessage = ex.Message
+                };
+                return View("Error", model);
             }
-
-            return View(employee);
+            
         }
 
         // POST: Employees/Delete/5
@@ -165,18 +226,31 @@ namespace EmployeeManagementSystem.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Employees == null)
+            try
             {
-                return Problem("Entity set 'EMSDbContext.Employees'  is null.");
-            }
-            var employee = await _context.Employees.FindAsync(id);
-            if (employee != null)
-            {
-                _context.Employees.Remove(employee);
-            }
+                if (_context.Employees == null)
+                {
+                    return Problem("Entity set 'EMSDbContext.Employees'  is null.");
+                }
+                var employee = await _context.Employees.FindAsync(id);
+                if (employee != null)
+                {
+                    _context.Employees.Remove(employee);
+                }
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+
+            }
+            catch (Exception ex)
+            {
+                var model = new ErrorViewModel
+                {
+                    ErrorMessage = ex.Message
+                };
+                return View("Error", model);
+            }
+            
         }
 
         private bool EmployeeExists(int id)
@@ -189,66 +263,91 @@ namespace EmployeeManagementSystem.Controllers
 
         public IActionResult Search(string input)
         {
-            int data;
-            bool IsInteger = int.TryParse(input, out data);
+            try
+            {
+                int data;
+                bool IsInteger = int.TryParse(input, out data);
 
-            if (IsInteger)
-            {
-                var result = _context.Employees.FirstOrDefault(x=> x.EmployeeId == data);
-                var modelList = new List<Employee> { result };
-                return View("Index",modelList);
+                if (IsInteger)
+                {
+                    var result = _context.Employees.FirstOrDefault(x => x.EmployeeId == data);
+                    var modelList = new List<Employee> { result };
+                    return View("Index", modelList);
+                }
+                else
+                {
+                    var result = _context.Employees.Where(item =>
+                    EF.Functions.Like(item.FirstName, $"%{input}%") ||
+                    EF.Functions.Like(item.LastName, $"%{input}%") ||
+                    EF.Functions.Like(item.Designation, $"%{input}%") ||
+                    EF.Functions.Like(item.OfficeLocation, $"%{input}%") ||
+                    EF.Functions.Like(item.MobileNumber, $"%{input}%") ||
+                    EF.Functions.Like(item.Gender, $"%{input}%") ||
+                    EF.Functions.Like(item.DateOfBirth.ToString(), $"%{input}%") ||
+                    EF.Functions.Like(item.Manager, $"%{input}%")
+                    ).ToList();
+                    return View("Index", result);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                var result = _context.Employees.Where(item =>
-                EF.Functions.Like(item.FirstName, $"%{input}%") ||
-                EF.Functions.Like(item.LastName, $"%{input}%") ||
-                EF.Functions.Like(item.Designation, $"%{input}%") ||
-                EF.Functions.Like(item.OfficeLocation, $"%{input}%") ||
-                EF.Functions.Like(item.MobileNumber, $"%{input}%") ||
-                EF.Functions.Like(item.Gender, $"%{input}%") ||
-                EF.Functions.Like(item.DateOfBirth.ToString(), $"%{input}%") ||
-                EF.Functions.Like(item.Manager, $"%{input}%") 
-                ).ToList();
-                return View("Index",result);
+                var model = new ErrorViewModel
+                {
+                    ErrorMessage = ex.Message
+                };
+                return View("Error", model);
             }
+            
         }
 
-        // Implementing the search operation with constraints
+        /* ----- Implementing the search operation with constraints ----- */
 
         // First search box must only return results regarding the First Name
         // Second search box must only return results regarding the Last Name
-        // Both the search boxes have a search button
+        // Both the search boxes have a search button in common
+        // If both first name and last name is entered the matching result of a individual row should be displayed
 
         public IActionResult ModifiedSearch(string fname, string lname)
         {
-
-            var fnameresult = _context.Employees.Where(item =>
+            try
+            {
+                var fnameresult = _context.Employees.Where(item =>
                 EF.Functions.Like(item.FirstName, $"%{fname}%")
                 ).ToList();
-            var lnameresults = _context.Employees.Where(item =>
-                EF.Functions.Like(item.LastName, $"%{lname}%")
-                ).ToList();
-            
+                var lnameresults = _context.Employees.Where(item =>
+                    EF.Functions.Like(item.LastName, $"%{lname}%")
+                    ).ToList();
 
-            if (string.IsNullOrEmpty(fname) && string.IsNullOrEmpty(lname))
-            {
+
+                if (string.IsNullOrEmpty(fname) && string.IsNullOrEmpty(lname))
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else if (!string.IsNullOrEmpty(fname) && !string.IsNullOrEmpty(lname))
+                {
+                    var result = fnameresult.Where(x => x.LastName.Contains(lname));
+                    return View("Index", result);
+                }
+                else if (!string.IsNullOrEmpty(fname))
+                {
+                    return View("Index", fnameresult);
+                }
+                else if (!string.IsNullOrEmpty(lname))
+                {
+                    return View("Index", lnameresults);
+                }
                 return RedirectToAction(nameof(Index));
             }
-            else if(!string.IsNullOrEmpty(fname) && !string.IsNullOrEmpty(lname))
+            catch (Exception ex)
             {
-                var result = fnameresult.Where(x => x.LastName.Contains(lname));
-                return View("Index", result);
+                var model = new ErrorViewModel
+                {
+                    ErrorMessage = ex.Message
+                };
+                return View("Error", model);
             }
-            else if(!string.IsNullOrEmpty(fname))
-            {
-                return View("Index", fnameresult);
-            }
-            else if(!string.IsNullOrEmpty(lname))
-            {
-                return View("Index", lnameresults);
-            }
-            return RedirectToAction(nameof(Index));
+
+            
         }
         
     }
